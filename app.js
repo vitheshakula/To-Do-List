ecf7da7426ca606bd5a7b02221f07fd5f4d88c0c
vitheshakula <vithesh9104@gmail.com>
2025-08-10 08:41:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let items = []; // [{ name: "task", completed: false }]
+let items = []; 
 
 app.get("/", function (req, res) {
     res.render("list", { ejes: items });
